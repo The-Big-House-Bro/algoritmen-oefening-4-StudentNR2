a16e7d99ba173eb1213bc6963492e4c36ba2f17a
NR <nino.reyns@leerling.athenea.be>
2025-04-01 15:29:34 +0200

    Oefening gemaakt.

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,16 @@
-def countTargetPairs():
+nums = [1,3,4,-2,-4,6]
+target = 3
+
+def countTargetPairs(lijst, target):
+    resultaat = 0
+    i = 0
+    for x in lijst:
+        j = 0
+        for y in lijst:
+            if x + y < target and i < j:
+                resultaat += 1
+            j += 1
+        i += 1
+    return resultaat
+
+print(countTargetPairs(nums, target))
